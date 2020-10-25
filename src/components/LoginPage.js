3db4e5f40183add48307fd681064a79279f75635
sammacfarlane23 <sammacfarlane23@icloud.com>
2020-10-25 10:53:52 +0000

    Added new favicon

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import Button from 'react-bootstrap/Button';
 import { redirectUrlToSpotifyForLogin } from '../spotifyFunctions';
 
 export default () => {
   return (
     <div className='login-page'>
       <div className='login-page__box'>
-        <h1>Spotifire</h1>
+        <h1>Spotifyre</h1>
         <p>Login to see your most played tracks and artists on Spotify</p>
         <a href={redirectUrlToSpotifyForLogin()}>Continue with Spotify</a>
       </div>
