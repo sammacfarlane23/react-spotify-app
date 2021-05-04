1c61102a46e2185f1df59516df81abcd627a8e4c
Sam MacFarlane <sam@Sams-MacBook-Air.local>
2021-05-04 19:56:40 +0100

    Revert "Styling tweaks"
    This reverts commit 0453288b04d3662c689b2aa338c1c45e9127a42b.
    
    To fix app

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,25 +1,22 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
 import ItemImage from './ItemImage';
 
 const ItemList = ({ topList }) => {
   return (
-    <div className='d-flex'>
-      <div className='item-list'>
-        {topList.map((topItem, index) => (
-          <ItemImage
-            key={topItem.id}
-            item={topItem}
-            index={index}
-            // If a list item contains artists that means it is a track not an artist
-            src={
-              topItem.artists
-                ? topItem.album.images[0].url
-                : topItem.images[0].url
-            }
-          />
-        ))}
-      </div>
+    <div className='d-flex flex-row flex-wrap justify-content-center align-items-space-between mb-3'>
+      {topList.map((topItem, index) => (
+        <ItemImage
+          key={topItem.id}
+          item={topItem}
+          index={index}
+          // If a list item contains artists that means it is a track not an artist
+          src={
+            topItem.artists
+              ? topItem.album.images[0].url
+              : topItem.images[0].url
+          }
+        />
+      ))}
     </div>
   );
 };
