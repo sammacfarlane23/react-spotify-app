57e3bbe3ee420f906acfd3da408565839935d6bf
Sam MacFarlane <sam@Sams-MacBook-Air.local>
2022-08-27 15:47:33 +0100

    fix: Further improve playlist merge form UI

--- src/components/PlaylistMergeForm/index.js
+++ src/components/PlaylistMergeForm/index.js
@@ -15,14 +15,14 @@ const useStyles = makeStyles((theme) => ({
     "& .MuiOutlinedInput-root": {
       "& fieldset": {
         // - The <fieldset> inside the Input-root
-        borderColor: "#b2b2b2", // - Set the Input border
+        borderColor: "#fff", // - Set the Input border
       },
       "&:hover fieldset": {
-        borderColor: "#1db954", // - Set the Input border when parent has :hover
+        borderColor: "#fff", // - Set the Input border when parent has :hover
       },
       "&.Mui-focused fieldset": {
         // - Set the Input border when parent is focused
-        borderColor: "#1db954",
+        borderColor: "#fff",
       },
     },
   },
@@ -31,22 +31,33 @@ const useStyles = makeStyles((theme) => ({
     "& .MuiOutlinedInput-root": {
       "& fieldset": {
         // - The <fieldset> inside the Input-root
-        borderColor: "#b2b2b2", // - Set the Input border
+        borderColor: "#fff", // - Set the Input border
       },
       "&:hover fieldset": {
-        borderColor: "#1db954", // - Set the Input border when parent has :hover
+        borderColor: "#fff", // - Set the Input border when parent has :hover
       },
       "&.Mui-focused fieldset": {
         // - Set the Input border when parent is focused
-        borderColor: "#1db954",
+        borderColor: "#fff",
       },
     },
   },
   addPlaylistButton: {
+    border: "2px solid #fff",
     "&:hover": {
       border: "2px solid #fff",
-      backgroundColor: "#1db954",
+      color: "black",
+      backgroundColor: "#fff",
     },
+  },
+  createNewPlaylistButton: {
+    "&:hover": {
+      backgroundColor: "#fff",
+      color: "#000",
+    },
+  },
+  deleteButton: {
+    cursor: "pointer",
   },
 }));
 
@@ -63,127 +74,167 @@ const PlaylistMergeForm = ({
 
   const classes = useStyles();
 
-  // @TODO Make number of playlists adjustable
   return (
-    <Paper
-      variant="elevation"
-      elevation={20}
-      sx={{ backgroundColor: "#212121", mt: 5 }}
+    <Box
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
     >
-      <Box
-        component="form"
-        onSubmit={async (e) => {
-          e.preventDefault();
-          await createNewPlaylist(playlists);
+      <Paper
+        variant="elevation"
+        elevation={20}
+        sx={{
+          // backgroundColor: "#212121",
+          backgroundImage: "linear-gradient(to bottom left, #000, #313131)",
+          mt: 5,
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          width: "100%",
+          maxWidth: { md: "80%" },
         }}
-        sx={{ p: 4 }}
-        display="flex"
-        flexDirection="column"
       >
-        <Typography variant="h1" sx={{ mb: 3, fontWeight: "500" }} color="#fff">
-          Merge Your Playlists
-        </Typography>
+        <Box
+          component="form"
+          onSubmit={async (e) => {
+            e.preventDefault();
+            await createNewPlaylist(playlists);
+          }}
+          sx={{ p: 4, maxWidth: { md: 500 }, width: { md: "100%" } }}
+          display="flex"
+          flexDirection="column"
+        >
+          <Typography
+            variant="h1"
+            sx={{ mb: 3, fontWeight: "500" }}
+            color="#fff"
+          >
+            <span style={{ color: "#1db954" }}>Merge</span> Your Playlists
+          </Typography>
 
-        <TextField
-          label="New playlist name"
-          value={playlistName}
-          onChange={(e) => setPlaylistName(e.target.value)}
-          placeholder="Your new playlist name"
-          classes={{ root: classes.textFieldRoot }}
-          sx={{ backgroundColor: "#414141", mb: 3 }}
-        />
+          <TextField
+            label="New playlist name"
+            value={playlistName}
+            onChange={(e) => setPlaylistName(e.target.value)}
+            placeholder="A great name for your new playlist..."
+            classes={{ root: classes.textFieldRoot }}
+            sx={{ backgroundColor: "#414141", mb: 5 }}
+          />
+          <Box textAlign="center">
+            <Typography
+              variant="h6"
+              sx={{ mb: 2, fontWeight: "500" }}
+              color="#fff"
+            >
+              {playlists.length === 0 ? (
+                <>Click the button below select playlists to merge</>
+              ) : (
+                <>Playlists to Merge</>
+              )}
+            </Typography>
+          </Box>
 
-        <Typography variant="h6" sx={{ mb: 3, fontWeight: "500" }} color="#fff">
-          Playlists to merge
-        </Typography>
-        {playlists.map((playlist, playlistIndex) => (
-          <FormGroup
+          <Box sx={{ mb: 5 }}>
+            {playlists.map((playlist, playlistIndex) => (
+              <FormGroup
+                sx={{
+                  display: "flex",
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                }}
+              >
+                <TextField
+                  id="standard-select-currency-native"
+                  select={true}
+                  label={`Playlist ${playlistIndex + 1}`}
+                  classes={{ root: classes.selectFieldRoot }}
+                  onChange={(e) => {
+                    const firstPlaylistObj = find(items, {
+                      id: e.target.value,
+                    });
+                    const newArray = [...playlists];
+                    newArray[playlistIndex] = firstPlaylistObj;
+                    setPlaylists(newArray);
+                  }}
+                  SelectProps={{
+                    native: true,
+                  }}
+                  // variant="standard"
+                  sx={{ backgroundColor: "#414141", mb: 3, flexGrow: 1, mr: 2 }}
+                >
+                  {items.map(({ name, id }, index) => (
+                    <option
+                      selected={playlistIndex === index}
+                      key={id}
+                      value={id}
+                    >
+                      {name}
+                    </option>
+                  ))}
+                </TextField>
+                <Box
+                  display="flex"
+                  flexDirection="column"
+                  justifyContent="center"
+                  mb={3}
+                >
+                  <FontAwesomeIcon
+                    size="md"
+                    icon={faTrash}
+                    className={classes.deleteButton}
+                    // @TODO Get the removal of the playlist to work
+                    onClick={() => {
+                      const newPlaylists = playlists.filter(
+                        (playlist, index) => index !== playlistIndex
+                      );
+                      setPlaylists(newPlaylists);
+                    }}
+                  />
+                </Box>
+              </FormGroup>
+            ))}
+          </Box>
+
+          <Button
             sx={{
-              display: "flex",
-              flexDirection: "row",
-              justifyContent: "space-between",
+              alignSelf: "center",
+              maxWidth: 180,
+              border: "1px solid #fff",
+              py: 1,
+              mb: 3,
+            }}
+            className={classes.addPlaylistButton}
+            variant="outlined"
+            onClick={() => {
+              setPlaylists([...playlists, { playlist: null }]);
             }}
           >
-            <TextField
-              id="standard-select-currency-native"
-              select={true}
-              label={`Playlist ${playlistIndex + 1}`}
-              classes={{ root: classes.selectFieldRoot }}
-              onChange={(e) => {
-                const firstPlaylistObj = find(items, {
-                  id: e.target.value,
-                });
-                const newArray = [...playlists];
-                newArray[playlistIndex] = firstPlaylistObj;
-                setPlaylists(newArray);
+            +{"  "}Add playlist
+          </Button>
+          <Box textAlign="center">
+            <Button
+              sx={{
+                p: 2,
+                // maxWidth: 200,
+                color: "black",
+                backgroundColor: "primary",
+                fontSize: "1rem",
+                maxWidth: 400,
               }}
-              SelectProps={{
-                native: true,
-              }}
-              // variant="standard"
-              sx={{ backgroundColor: "#414141", mb: 3, flexGrow: 1, mr: 2 }}
+              className={classes.createNewPlaylistButton}
+              variant="contained"
+              fullWidth={true}
+              href="/"
+              type="submit"
             >
-              {items.map(({ name, id }, index) => (
-                <option selected={playlistIndex === index} key={id} value={id}>
-                  {name}
-                </option>
-              ))}
-            </TextField>
-            <Box
-              display="flex"
-              flexDirection="column"
-              justifyContent="center"
-              mb={3}
-            >
-              <FontAwesomeIcon
-                size="md"
-                icon={faTrash}
-                // @TODO Get the removal of the playlist to work
-                onClick={() => {
-                  const newPlaylists = playlists.filter(
-                    (playlist, index) => index !== playlistIndex
-                  );
-                  setPlaylists(newPlaylists);
-                }}
-              />
-            </Box>
-          </FormGroup>
-        ))}
-
-        <Button
-          sx={{
-            alignSelf: "center",
-            maxWidth: 180,
-            border: "1px solid #fff",
-            py: 1,
-          }}
-          className={classes.addPlaylistButton}
-          variant="outlined"
-          onClick={() => {
-            setPlaylists([...playlists, { playlist: null }]);
-          }}
-        >
-          +{"  "}Add playlist
-        </Button>
-        <Button
-          sx={{
-            mt: 3,
-            p: 2,
-            // maxWidth: 200,
-            color: "black",
-            backgroundColor: "#fff",
-            fontSize: "1rem",
-          }}
-          variant="contained"
-          fullWidth={true}
-          href="/"
-          className="text-black"
-          type="submit"
-        >
-          Create new playlist
-        </Button>
-      </Box>
-    </Paper>
+              Create new playlist
+            </Button>
+          </Box>
+        </Box>
+      </Paper>
+    </Box>
   );
 };
 
